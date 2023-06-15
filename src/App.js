a80d754e55cefb57bc30243455d182995302fc97
AlexElguetaDev <alexelgueta.developer@gmail.com>
2023-06-14 22:39:39 -0400

    Renderizar listas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true,
+  },
+];
 
 export default function App() {
+  if (typeof notes === 'undefined' || notes.length === 0) {
+    return 'No tenemos notas que mostrar';
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {notes.map((note) => (
+        <div key={note.id}>
+          <h1>{note.content}</h1>
+          <p>{note.date}</p>
+        </div>
+      ))}
     </div>
   );
 }
